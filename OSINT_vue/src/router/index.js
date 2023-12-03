7b949b9e32d6c466b6008ae143d74682bf7ec622
BoyNextDoor♂ <59304676+RSSYLY@users.noreply.github.com>
2023-12-03 23:28:01 +0800

    安装并配置了tailwind，写了个页头

--- OSINT_vue/src/router/index.js
+++ OSINT_vue/src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/login/LoginView.vue'
+import SignonView from "@/views/login/SignonView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'home',
-      component: HomeView
+      component: LoginView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/signon',
+      name: 'signon',
+      component: SignonView
     }
   ]
 })
