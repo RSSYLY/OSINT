9af770aecc2d23ee34453c075283738bbbd10a20
Fan <477706421@qq.com>
2023-12-25 21:34:47 +0800

    mod

--- OSINT/urls.py
+++ OSINT/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
 import OSINT_DB.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/get_all_object', OSINT_DB.views.get_all_objects)
+    path('api/get_all_objects', OSINT_DB.views.get_all_objects),
+    path('api/get_all_events', OSINT_DB.views.get_all_events),
+    path('api/add_event', OSINT_DB.views.add_event),
+    path('api/update_event/<int:event_id>', OSINT_DB.views.update_event),
+    path('api/delete_event/<int:event_id>', OSINT_DB.views.delete_event),
+    path('api/add_object', OSINT_DB.views.add_object),
+    path('api/update_object/<int:object_id>', OSINT_DB.views.update_object),
+    path('api/delete_object/<int:object_id>', OSINT_DB.views.delete_object),
+    path('api/get_all_keywords', OSINT_DB.views.get_all_keywords),
+    path('api/add_keyword', OSINT_DB.views.add_keyword),
+    path('api/update_keyword/<int:keyword_id>', OSINT_DB.views.update_keyword),
+    path('api/delete_keyword/<int:keyword_id>', OSINT_DB.views.delete_keyword),
 ]
