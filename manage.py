fe438a48bd78ad3737818391907bce8ec19d30f7
BoyNextDoor♂ <59304676+RSSYLY@users.noreply.github.com>
2024-01-08 19:07:46 +0800

    增加个banner

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@
 import os
 import sys
 
-from Utils.Creeper import Banner
-
+from Utils.Enderdragon import banner
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OSINT.settings')
     try:
         from django.core.management import execute_from_command_line
+        banner()
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
